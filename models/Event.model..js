b5e60dea0846e785fa1342a612006677b22738e9
Lisa Hilterhaus <lisa@hilterhaus.net>
2022-08-03 16:18:35 +0200

    models

---
+++ models/Event.model..js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema(
+  {
+    name: {
+      type: String, 
+      required: [true, 'Please provide a name for your event.'],
+    },
+    date: {
+      type: Date,
+      min: () => Date.now()
+    },
+    location: {
+      street: String, 
+      housenumber: Number,
+      citycode: Number, 
+      city: String,
+      country: String
+    }, 
+    participants: {
+      type: [{type: Schema.Types.ObjectId, ref: 'User'}]
+    }
+    , 
+    threads: [{type: Schema.Types.ObjectId, ref: "Threads"}],
+    polls: [{type: Schema.Types.ObjectId, ref: "Polls"}],
+    organizer: [{type: Schema.Types.ObjectId, ref: 'User'}]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Event", eventSchema);
