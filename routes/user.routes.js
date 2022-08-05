f5459ca0ad617a5c2b4ec0ee8c5360fca1f5d9bf
Lara Rockenstein <lara.rockenstein@live.de>
2022-08-05 17:11:03 +0200

    user routes... filter by req.query

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,17 +4,33 @@ const mongoose = require("mongoose");
 const User = require("../models/User.model");
 
 router.get("/users", (req, res) => {
-    User.find()
+
+    console.log("req.query iiiiiis:");
+    console.log(req.query)
+    console.log(req.query.ids)
+
+    if (!req.query.ids) {
+        User.find()
         .then(user => {
             res.json(user);
         })
         .catch(err => {res.json(err)})
+    }
+
+    else {
+        User.find({_id: req.query.ids})
+        .then(user => {
+            res.json(user);
+        })
+        .catch(err => {res.json(err)})
+    }
 });
 
 router.get("/users/:userId", (req, res) => {
     const { userId } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(userId)) {
+
         res.status(400).json({ message: 'Specified id is not valid' });
         return;
     }
@@ -23,5 +39,7 @@ router.get("/users/:userId", (req, res) => {
         .then(user => res.status(200).json(user))
         .catch(error => res.json(error));
 });
+
+
 
 module.exports = router;
